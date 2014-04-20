a6489d61e76bdef5a3f5cb55eccddc7dc2eef882
barbgrecco <barbara@thegreccos.com>
2014-04-20 15:38:59 -0700

    Finished Bitmaps

--- gen/com/example/hw251bgrecco/R.java
+++ gen/com/example/hw251bgrecco/R.java
@@ -21,16 +21,24 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logonologo=0x7f020001;
+        public static final int buttonfocus=0x7f020000;
+        public static final int buttonnormal=0x7f020001;
+        public static final int buttonpressed=0x7f020002;
+        public static final int control_button=0x7f020003;
+        public static final int control_edittext=0x7f020004;
+        public static final int editnormal=0x7f020005;
+        public static final int editselected=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int logonologo=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int email=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int email=0x7f080003;
         public static final int enteredEmail=0x7f080000;
         public static final int enteredPassword=0x7f080001;
-        public static final int password=0x7f080003;
-        public static final int signin=0x7f080004;
+        public static final int image=0x7f080002;
+        public static final int password=0x7f080004;
+        public static final int signin=0x7f080005;
     }
     public static final class layout {
         public static final int activity_info=0x7f030000;
@@ -43,6 +51,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int dummy=0x7f050007;
         public static final int email=0x7f050004;
         public static final int hello_world=0x7f050002;
         public static final int password=0x7f050005;
